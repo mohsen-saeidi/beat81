c0a6322c6b7310149288cdf27cea10e8533a17d0
Mohsen Saeidi <mohsen.saeidi@hoffmann-group.com>
2025-11-07 22:21:54 +0100

    book from next week on

--- beat81/job_schedule.py
+++ beat81/job_schedule.py
@@ -1,6 +1,8 @@
+from datetime import timedelta
+
 from apscheduler.schedulers.background import BackgroundScheduler
 from apscheduler.triggers.cron import CronTrigger
-from pytz import utc
+
 from beat81.beat81_api import register_recursive, find_next_event
 from beat81.city_helper import City
 from beat81.date_helper import DaysOfWeek, next_date_time_weekday
@@ -18,12 +20,12 @@ def job():
         time = subscription.get('time')
         hour = time.split(":")[0]
         minute = time.split(":")[1]
-        next_date = next_date_time_weekday(day_of_week, int(hour), int(minute))
-        next_event = find_next_event(city, location_id, next_date)
-        register_recursive(next_event.get('id'), telegram_user_id, city, location_id, next_date)
+        date = next_date_time_weekday(day_of_week, int(hour), int(minute)) + timedelta(days=7)
+        next_event = find_next_event(city, location_id, date)
+        register_recursive(next_event.get('id'), telegram_user_id, city, location_id, date)
 
 
 def init_scheduler():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(job, CronTrigger(hour="22", minute="15", second="0"))
+    scheduler.add_job(job, CronTrigger(hour="21", minute="0", second="0"))
     scheduler.start()
